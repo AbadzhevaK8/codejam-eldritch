bade669002657fa217ebb7f854cce7200c3331dd
k8 <make.me.loco@gmail.com>
2022-08-22 23:44:45 +0300

    feat: add html and css features.

--- assets/Ancients/index.js
+++ assets/Ancients/index.js
@@ -1,7 +1,7 @@
-import azathoth from './Azathoth.png';
-import cthulhu from './Cthulthu.png';
-import iogSothoth from './IogSothoth.png';
-import shubNiggurath from './ShubNiggurath.png';
+const azathoth = document.querySelector('.Azathoth').src = './Azathoth.png'; 
+const cthulhu = document.querySelector('.Cthulthu').src = './Cthulthu.png';
+const iogSothoth = document.querySelector('.IogSothoth').src = './IogSothoth.png';
+const shubNiggurath = document.querySelector('.ShubNiggurath').src = './ShubNiggurath.png';
 
 const ancients = {
   azathoth,
